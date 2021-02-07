7afeaffdb06f4b6e3a2e4b6583d51d04423fdc9f
Emil <emilfigzal@gmail.com>
2021-02-07 17:05:19 +0100

    tests with exception

--- src/test/java/pl/emil7f/cart/CartServiceTest.java
+++ src/test/java/pl/emil7f/cart/CartServiceTest.java
@@ -8,6 +8,7 @@ import pl.emil7f.order.OrderStatus;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
@@ -125,5 +126,22 @@ class CartServiceTest {
         assertThat(resultCart.getOrders(), hasSize(1));
         assertThat(resultCart.getOrders().get(0).getOrderStatus(), equalTo(OrderStatus.PREPARING));
     }
+
+    @Test
+    void canHandleCartShouldThrowException() {
+        // given
+        Order order = new Order();
+        Cart cart = new Cart();
+        cart.addOrderToCart(order);
+
+        CartHandler cartHandler = mock(CartHandler.class);
+        CartService cartService = new CartService(cartHandler);
+
+        given(cartHandler.canHandleCart(cart)).willThrow(IllegalStateException.class);
+
+        // when
+        //then
+        assertThrows(IllegalStateException.class, () -> cartService.processCart(cart));
+         }
 
 }
