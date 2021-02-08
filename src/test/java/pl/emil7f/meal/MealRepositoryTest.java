b1e33f8ab1c09c08c3db2c5c3ab37b8db1c35893
Emil <emilfigzal@gmail.com>
2021-02-08 20:38:04 +0100

    findByNameByStartingLetters method tdd

--- src/test/java/pl/emil7f/meal/MealRepositoryTest.java
+++ src/test/java/pl/emil7f/meal/MealRepositoryTest.java
@@ -39,17 +39,35 @@ public class MealRepositoryTest {
     }
 
     @Test
-    void shouldBeAbleToFindMealByName() {
+    void shouldBeAbleToFindMealByExactName() {
 
         // given
         Meal meal = new Meal(10, "Pizza");
+        Meal meal2 = new Meal(10, "Pi");
+
         mealRepository.add(meal);
+        mealRepository.add(meal2);
         // when
-        List<Meal> results = mealRepository.findByName("Pizza");
+        List<Meal> results = mealRepository.findByName("Pizza", true);
 
         //then
         assertThat(results.size(), is(1));
 
+    }
+
+    @Test
+    void shouldBeAbleToFindNameByStartingLetters(){
+        // given
+        Meal meal = new Meal(10, "Pizza");
+        Meal meal2 = new Meal(10, "Pi");
+
+        mealRepository.add(meal);
+        mealRepository.add(meal2);
+        // when
+        List<Meal> results = mealRepository.findByName("P", false);
+
+        //then
+        assertThat(results.size(), is(2));
     }
 
     @Test
